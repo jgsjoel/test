cb8bb3f69f46e986452f060d646369b7b74719b8
Joel <your_email@example.com>
2025-08-02 15:09:51 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jgsjoel/test
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		response := "hello from port 3000"
+		w.Write([]byte(response))
+	})
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		panic(err)
+	}
+
+}

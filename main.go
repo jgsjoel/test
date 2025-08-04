398cfc03e72f236a1b53d3a90b5a4647dfcea07a
Joel Smith <89963423+jgsjoel@users.noreply.github.com>
2025-08-04 16:56:07 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		response := "hello from port 3000"
+		response := "hello from port 8001"
 		w.Write([]byte(response))
 	})
 
-	println("listening at port 3000")
-	err := http.ListenAndServe(":3000", nil)
+	println("listening at port 8001")
+	err := http.ListenAndServe(":8001", nil)
 	if err != nil {
 		panic(err)
 	}

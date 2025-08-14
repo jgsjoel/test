4538fe2081434e052fd40bfd6e7a2b9d41476cdb
Joel <your_email@example.com>
2025-08-14 23:26:28 +0530

    redis added

--- main.go
+++ main.go
@@ -1,16 +1,43 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
 
 func main() {
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // change if running elsewhere
+		Password: "",               // no password by default
+		DB:       0,                // default DB
+	})
+
+	err := rdb.Set(ctx, "greeting", "Hello Redis from Go!", 0).Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	val, err := rdb.Get(ctx, "greeting").Result()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Stored value:", val)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		response := "hello from port 3000"
 		w.Write([]byte(response))
 	})
 
-	println("listening at port 3000")
-	err := http.ListenAndServe(":3000", nil)
+	println("listening at port 8001")
+	err = http.ListenAndServe(":8001", nil)
 	if err != nil {
 		panic(err)
 	}
